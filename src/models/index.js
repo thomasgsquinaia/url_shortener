8ca24fa971d6ed1744dbf0f8e65581eee7e9d020
thomasgsquinaia <thomasquinaia@gmail.com>
2024-06-17 11:42:46 -0300

    [main] => fix: models index

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,11 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: 'true'
+    }
+  }
 });
 
 const User = require('./user')(sequelize);
