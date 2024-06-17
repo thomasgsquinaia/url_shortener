ae68a1efea0cab3ebd1fdddea1257f9a7ed4ae65
thomasgsquinaia <thomasquinaia@gmail.com>
2024-06-17 11:11:12 -0300

    [main] => feat: add swagger

---
+++ src/swagger.js
@@ -0,0 +1,40 @@
+const swaggerAutogen = require("swagger-autogen")({ openapi: '3.0.0' });
+
+const doc = {
+  info: {
+    version: "1.0",
+    title: "Url Shortner Api Rest",
+    description: "API REST to shorten URLs",
+  },
+  servers: [
+    { url: 'http://localhost:3001', description: "LocalTest" },
+    { url: 'http://localhost:3001', description: "LocalProd" },
+  ],
+  consumes: ["application/json", "multipart/form-data"],
+  produces: ["application/json"],
+  tags: [
+    { name: "auth", description: "Endpoints of auth" },
+    { name: "url", description: "Endpoint of url" }
+  ],
+  components: {
+    securitySchemes: {
+      Bearer: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        description: `Enter the token with the 'Bearer ' prefix, e.g. "Bearer abcde12345".`,
+      },
+    },
+  },
+  security: [
+    {
+      Bearer: [],
+    },
+  ],
+  definitions: {},
+};
+
+const outputFile = "src/swagger.json";
+const endpointsFiles = ["./app.js"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
